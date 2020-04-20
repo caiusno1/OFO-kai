74174ef1cc46a3a5ffb6ca4d06c167027a1f2093
Kai Biermeier <kbiermeier1@web.de>
2020-04-20 05:42:09 +0200

    adjust server db queryies + api interface

--- api-server/services/mongo/UserService.js
+++ api-server/services/mongo/UserService.js
@@ -1,4 +1,4 @@
-
+const ObjectId = require("mongoose").Types.ObjectId;
 module.exports = class UserService {
     userModel;
     username;
@@ -14,31 +14,33 @@ module.exports = class UserService {
         this.eventModel = EventModel;
     }
     getFriends(){
-        return this.userModel.find({name:this.username}).populate('friends').then((res)=>{
-            return Promise.resolve(res.friends.map((friend)=>{return {name:friend.name,id:friend._id}}));
+        return this.userModel.findOne({name:this.username}).populate('friends').then((res)=>{
+            return res.friends.map((friend)=>{return {name:friend.name,id:friend._id}});
         });
     }
     getProfile(){
         return this.userModel.findOne({name:this.username}).then((res)=>{
             // console.log(res);
-            return Promise.resolve({name:res.name,age:res.age, hobbies:res.hobbies, job:res.job, aboutMe: res.ueberMich});
+            return {name:res.name,age:res.age, hobbies:res.hobbies, job:res.job, aboutMe: res.ueberMich};
         });
     }
     getMyEvents(){
-        return this.eventModel.find().populate({
+        return this.eventModel.find({$or:[{participants:[this.userID]},{organiser:this.userID}]})
+        .populate({
             path: "participants",
-            match: { name: this.username },
-            select: "name"
-        }).then((res)=>{
+        })
+        .then((res)=>{
             let myEvents = [];
             for(const event of res){
-                myEvents.push({topic: event.topic, date: event.date, time: event.time, participants: event.participants});
+                myEvents.push({topic: event.topic, date: event.date, time: event.time, participants: event.participants.map((participant)=> {return {name:participant.name, id:participant.id}}), service:event.platform});
             }
             return myEvents;
+        }).catch(err => {
+            console.log(`Error:${err}`);
         });
     }
-    addToMyEvents(topic, date, time, participants){
-        const newEvent = new EventModel({topic:topic, date: date, time: time, participants: participants.map(participant => id)})
-        newEvent.save().then(() => console.log('Event saved'));
+    addToMyEvents(event){
+        const newEvent = new this.eventModel({topic:event.topic, date: event.date, time: event.time, participants: event.participants.map(participant => ObjectId(participant.id)), organiser: this.userID, platform: event.platform})
+        return newEvent.save().then(() => console.log('Event saved'));
     }
 }
